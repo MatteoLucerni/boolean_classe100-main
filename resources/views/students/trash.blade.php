37c1380ed59e92ce5cc04a7a7e0279972d17cf1f
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-30 16:47:59 +0200

    added form to delete all

--- resources/views/students/trash.blade.php
+++ resources/views/students/trash.blade.php
@@ -6,13 +6,20 @@
         <h2>TRASH</h2>
         <ul>
             @forelse ($students as $student)
-                <li>
+                <li class="card my-3 p-4">
                     <h1>{{ $student->first_name }} {{ $student->last_name }}</h1>
                 </li>
             @empty
                 <h1 class="text-danger">Non ci sono studenti nel cestino</h1>
             @endforelse
         </ul>
+        <form action="{{ route('students.dropAll') }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger">
+                SVUOTA CENSTINO
+            </button>
+        </form>
         <a href="{{ route('students.index') }}">Torna alla lista studenti</a>
     </div>
 @endsection
